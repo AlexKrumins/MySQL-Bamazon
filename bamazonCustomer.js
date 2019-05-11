0b7a83a66a1933b62374c18198144ef83f9878c1
Alex Krumins <justkrumins@gmail.com>
2019-05-10 22:27:37 -0500

    js files recommitted

---
+++ bamazonCustomer.js
@@ -0,0 +1,6 @@
+//display itmes available for sale
+//ids names prices
+//ask for id of product to buy
+//ask for quantity of product
+//check inventory and respond
+//update inventory
---
+++ bamazonManager.js
@@ -0,0 +1,9 @@
+//List menu Options
+//View products for sale
+//-list items
+//View Low Inventory
+//-List inventory under 5
+//Add to inventory
+//-Choose product and quantity
+//add new product
+//-Insert new product to table
