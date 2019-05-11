146954a0c2adce6f9ddaa4b70eb5a20a02c5141b
Alex Krumins <justkrumins@gmail.com>
2019-05-11 18:32:49 -0500

    database established

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
   price DECIMAL(10,2) NULL,
