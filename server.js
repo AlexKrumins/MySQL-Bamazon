ad738d05f75cdc5e4bd39ddf0d032b17378994dd
Alex Krumins <justkrumins@gmail.com>
2019-05-12 11:41:38 -0500

    customer interface basic, debug needed

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ function login() {
     .prompt({
       name: "customerOrManager",
       type: "list",
-      message: "Please choose your account to log in.",
-      choices: ["CUSTOMER", "MANAGER"]
+      message: "Please choose your account to log in.\n=======================================",
+      choices: ["CUSTOMER", "MANAGER", "EXIT"]
     })
     .then(function(answer) {
       if (answer.customerOrManager === "CUSTOMER") {
@@ -28,7 +28,7 @@ function login() {
           .prompt({
             name: "loginCredentials",
             type: "input",
-            message: "Please enter your password to login (hint: 'admin')",
+            message: "Please enter your password to login (hint: 'admin')\n=======================================",
             validate: function(value) {
                 if (value === "admin") {
                     manager();
@@ -36,177 +36,90 @@ function login() {
                 return false;
             }
           })
+      } else {
+          connection.end();
       }
-    })
-}
-function customer() {
-connection.query("SELECT * FROM auctions", function(err, results) {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_name);
-            }
-            return choiceArray;
-          },
-          message: "What auction would you like to place a bid in?"
-        },
-        {
-          name: "bid",
-          type: "input",
-          message: "How much would you like to bid?"
-        }
-      ])
-      .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
-            chosenItem = results[i];
-          }
-        }
-
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE auctions SET ? WHERE ?",
-            [
-              {
-                highest_bid: answer.bid
-              },
-              {
-                id: chosenItem.id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("Bid placed successfully!");
-              start();
-            }
-          );
-        }
-        else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
-          start();
-        }
-      });
-  });
-}
-
-// function to handle posting new items up for auction
-function postAuction() {
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
-      {
-        name: "item",
-        type: "input",
-        message: "What is the item you would like to submit?"
-      },
-      {
-        name: "category",
-        type: "input",
-        message: "What category would you like to place your auction in?"
-      },
-      {
-        name: "startingBid",
-        type: "input",
-        message: "What would you like your starting bid to be?",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      // when finished prompting, insert a new item into the db with that info
-      connection.query(
-        "INSERT INTO auctions SET ?",
-        {
-          item_name: answer.item,
-          category: answer.category,
-          starting_bid: answer.startingBid || 0,
-          highest_bid: answer.startingBid || 0
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("Your auction was created successfully!");
-          // re-prompt the user for if they want to bid or post
-          start();
-        }
-      );
     });
 }
-
-function bidAuction() {
-  // query the database for all items being auctioned
-  connection.query("SELECT * FROM auctions", function(err, results) {
+function customer() {
+connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
     inquirer
       .prompt([
         {
           name: "choice",
           type: "rawlist",
           choices: function() {
-            var choiceArray = [];
+            var productArray = [];
             for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_name);
+              productArray.push(results[i].product_name);
             }
-            return choiceArray;
+            return productArray;
           },
-          message: "What auction would you like to place a bid in?"
+          message: "Select the item you wish to purchase.\n======================================="
         },
         {
-          name: "bid",
+          name: "quantity",
           type: "input",
-          message: "How much would you like to bid?"
+          message: "How many units would you like to buy?\n=======================================",
+          validate: function(value) {
+            if (value === "" || 0) {
+                return false;
+            }
+            else if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
         }
       ])
       .then(function(answer) {
-        // get the information of the chosen item
         var chosenItem;
         for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
+          if (results[i].product_name === answer.choice) {
             chosenItem = results[i];
           }
         }
-
-        // determine if bid was high enough
-        if (chosenItem.highest_bid < parseInt(answer.bid)) {
-          // bid was high enough, so update db, let the user know, and start over
+        if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+          var newStock_quantity = chosenItem.stock_quantity - answer.quantity;
           connection.query(
-            "UPDATE auctions SET ? WHERE ?",
+            "UPDATE products SET ? WHERE ?",
             [
               {
-                highest_bid: answer.bid
+                stock_quantity: newStock_quantity,
               },
               {
-                id: chosenItem.id
+                item_id: chosenItem.item_id
               }
             ],
             function(error) {
               if (error) throw err;
-              console.log("Bid placed successfully!");
-              start();
+              console.log("Your Order is on the way! Your total will be $" + (chosenItem.price * answer.quantity) + "\n==================================================");
+              startOverPrompt();
             }
           );
         }
         else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Your bid was too low. Try again...");
-          start();
+          console.log("Unfortunately, there is not enough in stock to fulfill your order\n=================================================================");
+          startOverPrompt();
         }
       });
   });
 }
+
+function startOverPrompt() {
+    inquirer
+        .prompt({
+            name: "againOrLogout",
+            type: "list",
+            message: "Would you like to place another order?\n========================================",
+            choices: ["YES", "LOGOUT"]
+        })
+        .then(function(answer) {
+            if (answer.againOrLogout === "YES") {
+                customer();
+            } else if (answer.againOrLogout === "LOGOUT") {
+                login();
+            }
+        })
+};
