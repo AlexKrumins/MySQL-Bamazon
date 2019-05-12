af0e14ce4870dd714f682920a5aa1d61cf7beb66
Alex Krumins <justkrumins@gmail.com>
2019-05-12 09:36:24 -0500

    dependencies established

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 var mysql = require("mysql");
-
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "",
   database: "bamazon_db"
 });
@@ -18,14 +10,12 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  readColleges();
+  readProducts();
 });
 
-function readColleges() {
+function readProducts() {
   connection.query("SELECT name FROM colleges", function(err, res) {
     if (err) throw err;
-
-    // Log all results of the SELECT statement
     console.log(res);
     connection.end();
   });
