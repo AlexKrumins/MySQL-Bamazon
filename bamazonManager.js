eeafe9024caa19abf4884f7f587ed803821e410f
CupidPunch <47199557+AlexKrumins@users.noreply.github.com>
2019-05-12 23:55:05 -0500

    Delete bamazonManager.js

--- bamazonManager.js
+++
@@ -1,9 +0,0 @@
-//List menu Options
-//View products for sale
-//-list items
-//View Low Inventory
-//-List inventory under 5
-//Add to inventory
-//-Choose product and quantity
-//add new product
-//-Insert new product to table
